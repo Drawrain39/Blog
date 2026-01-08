b260ed6d892c53e3cd7d7e6dcfa08ff4dff5a1c2
Drawrain39 <858617595@qq.com>
2026-01-08 21:21:18 +0800

    修改了主题暗色展示

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,11 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Drawrain's Blog",
-	subtitle: "share",
+	subtitle: "对星辰倾诉一切",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 345, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: false,
